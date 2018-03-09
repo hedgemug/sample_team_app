827dfeb67095e067e5dca89e6c9f17f3c9f7a1a4
Irina <irenekochetkova@gmail.com>
2018-03-08 20:06:50 -0800

    added show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
 		render json: {message: "We need more Stephen in the Acutalize video!!!", name: "Dani"}
 	end
 
+  def show
+    render json: {message: "Show page has created!"}
+  end
+
 end
