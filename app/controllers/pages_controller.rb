771fe437f8ce49b20a9d7e76779f3efdc984d6b8
Irina <irenekochetkova@gmail.com>
2018-03-08 20:24:33 -0800

    fixed routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,11 @@ class PagesController < ApplicationController
 		render json: {message: "We need more Stephen in the Acutalize video!!!", name: "Dani"}
 	end
 
-<<<<<<< HEAD
-  def show
-    render json: {message: "Show page has created!"}
-  end
-=======
+
+  
+
 	def show
 		render json: {message: "You've reached the pages show page!"}
 	end
->>>>>>> b608e555dc72970c8f05eba1eeff06acc22d9847
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get '/pages' => 'pages#index'
-<<<<<<< HEAD
-  get '/pages/:id' => 'pages#index'
-=======
   get '/pages/:id' => 'pages#show'
->>>>>>> b608e555dc72970c8f05eba1eeff06acc22d9847
+
 end
